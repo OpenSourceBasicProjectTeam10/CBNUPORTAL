85106e1ec40d2bbc1091af2c6913ab8c64400549
shlee4522 <leeseunghun1234@naver.com>
2024-06-06 11:41:09 +0900

    구글맵 지도 추가

---
+++ g6/plugin/weather/config/keys.py
@@ -0,0 +1,5 @@
+# 이 파일을 keys.py 파일로 복사하여 아래의 설정값을 입력하세요.
+GOOGLE_MAPS_API_KEY = "AIzaSyC-ubMriEO7EcZR0QGTaPePUTELay0DIbI"
+OPENWEATHERMAP_API_KEY = "732a7cd512fceafd33062694550864b6"
+# X-NCP-APIGW-API-KEY-ID (Naver Cloud Platform Clinet ID)
+ncpClientId = "1fy48n0r1i"
