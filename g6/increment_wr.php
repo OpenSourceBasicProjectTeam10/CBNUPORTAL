1f766f0868c17f3e2603450ab3ba1caf23d1c6cf
kang taekwang <blognolnolja@gmail.com>
2024-06-06 15:01:00 +0900

    increase update
    increase update

--- g6/increment_wr.php
+++ g6/increment_wr.php
@@ -19,36 +19,27 @@ if ($conn->connect_error) {
 }
 
 // POST 데이터 확인
-if(isset($_POST['post_id']) && isset($_POST['member_id'])) {
+if(isset($_POST['post_id'])) {
     $postId = $_POST['post_id'];
-    $memberId = $_POST['member_id'];
 
     // wr_1 값이 성공적으로 업데이트된 경우, wr_1과 wr_2 값을 가져옴
-    $sql_select = "SELECT wr_1, wr_2, wr_4 FROM g6_CBNUPORTALwrite_contest WHERE wr_id = ?";
+    $sql_select = "SELECT wr_1, wr_2 FROM g6_CBNUPORTALwrite_contest WHERE wr_id = ?";
     $stmt_select = $conn->prepare($sql_select);
     $stmt_select->bind_param("i", $postId);
     $stmt_select->execute();
-    $stmt_select->bind_result($wr1, $wr2, $wr4);
+    $stmt_select->bind_result($wr1, $wr2);
     $stmt_select->fetch();
     $stmt_select->close();
-
-    // wr_4 배열 업데이트
-    $wr4Array = json_decode($wr4, true);
-    if (!$wr4Array) {
-        $wr4Array = [];
-    }
-    $wr4Array[$memberId] = "참가중";
 
     // wr_1과 wr_2 비교하여 초과 여부 확인
     if ($wr1 < $wr2) {
         // wr_1 값이 wr_2보다 작거나 같으면 wr_1을 1 증가시킴
-        $sql_update = "UPDATE g6_CBNUPORTALwrite_contest SET wr_1 = wr_1 + 1, wr_4 = ? WHERE wr_id = ?";
+        $sql_update = "UPDATE g6_CBNUPORTALwrite_contest SET wr_1 = wr_1 + 1 WHERE wr_id = ?";
         $stmt_update = $conn->prepare($sql_update);
-        $newWr4 = json_encode($wr4Array);
-        $stmt_update->bind_param("si", $newWr4, $postId);
+        $stmt_update->bind_param("i", $postId);
         
         if($stmt_update->execute()) {
-            echo json_encode(array('success' => true, 'wr_1' => $wr1 + 1, 'wr_2' => $wr2, 'wr_4' => $newWr4));
+            echo json_encode(array('success' => true, 'wr_1' => $wr1 + 1, 'wr_2' => $wr2));
         } else {
             echo json_encode(array('success' => false, 'error' => '데이터베이스 업데이트 실패: ' . $stmt_update->error));
         }
@@ -60,7 +51,7 @@ if(isset($_POST['post_id']) && isset($_POST['member_id'])) {
     }
 } else {
     // POST 데이터가 없는 경우
-    echo json_encode(array('success' => false, 'error' => '게시글 ID나 회원 ID가 전송되지 않았습니다.'));
+    echo json_encode(array('success' => false, 'error' => '게시글 ID가 전송되지 않았습니다.'));
 }
 
 // 데이터베이스 연결 종료
